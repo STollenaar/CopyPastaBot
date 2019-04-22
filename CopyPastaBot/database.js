bf2b179cad916d803a9a032c898a40fd9186464d
Stephen <1guitargun1@gmail.com>
2019-04-22 09:59:06 -0230

    initial commit

---
+++ CopyPastaBot/database.js
@@ -0,0 +1,29 @@
+﻿
+let sqlite = require('sqlite3').verbose();
+
+let db = new sqlite.Database('./COPY_DB.db');
+
+module.exports = {
+
+    defaultConfig(fs, callback) {
+        db.serialize(function () {
+            db.get(`SELECT * FROM config;`, (err, rows) => {
+                let object = [];
+                object.push({
+                    'AuthTkn': rows.AuthTkn,
+                    'Debug': rows.Debug,
+                    'DebugServer': rows.DebugServer,
+                    'IntervalTimeInSeconds': rows.IntervalTimeInSeconds,
+                    'User_Agent': rows.User_Agent,
+                    'Client_Id': rows.Client_Id,
+                    'Client_Secret': rows.Client_Secret,
+                    'Username': rows.Username,
+                    'Password': rows.Password,
+                    'MinUpvotes': rows.MinUpvotes
+                });
+                let json = JSON.stringify(object);
+                fs.writeFile('./config.json', json, 'utf8', callback);
+            });
+        });
+    }
+};
--- CopyPastaBot/server.js
+++ CopyPastaBot/server.js
@@ -1,8 +1,64 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+import { setInterval } from 'timers';
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const { Client, RichEmbed } = require('discord.js');
+const database = require('./database');
+const snoowrap = require('snoowrap');
+
+let config;
+let r;
+
+fs.stat('./config.json', function (err, stat) {
+    if (err === null) {
+        config = require('./config.json')[0];
+        r = new snoowrap({
+            userAgent: config.User_Agent,
+            clientId: config.Client_Id,
+            clientSecret: config.Client_Secret,
+            username: config.Username,
+            password: config.Password
+        })
+    } else if (err.code === 'ENOENT') {
+        console.log("Deploying config");
+        database.defaultConfig(fs, function () {
+            config = require('./config.json')[0];
+            console.log("Stopping app to get appropriate info");
+            stop();
+        });
+    }
+
+});
+
+
+// Initialize Discord Bot
+const client = new Client();
+
+client.on('ready', function (evt) {
+    console.log("Connected");
+
+    //setInterval(function () {
+
+
+    //}, config.IntervalTimeInSeconds*1000);
+
+});
+
+input.addListener("data", async function (d) {
+    d = d.toString().trim().split(" ");
+
+    switch (d[0]) {
+        case 'hot':
+            r.getSubreddit('copypasta').getHot(10).then((listing) => {
+                for (let sub in listing) {
+                    sub = listing[sub];
+                    console.log(sub);
+                }
+            });
+            break;
+    }
+});
+
+
+
+
+
+client.login(config.AuthTkn);
