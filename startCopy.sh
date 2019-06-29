a80f10ffe890d3f7b398fa5d950c22caea3254b1
STollenaar <stephen@tollenaar.com>
2019-06-29 13:44:28 +0000

    edited docker command

--- startCopy.sh
+++ startCopy.sh
@@ -5,4 +5,6 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
 docker build -t copypastabot "$SCRIPT_DIR"/CopyPastaBot 
 
+#docker run --rm --name espeakbox -d -p 8080:8080 parente/espeakbox
+
 docker run --rm -t -i copypastabot
