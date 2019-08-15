d9640fcfd0d50a13b5e50414efeb98ccba50b745
STollenaar <stephen@tollenaar.com>
2019-08-15 22:30:30 +0000

    added auto restart when failure occurs

--- startCopy.sh
+++ startCopy.sh
@@ -8,4 +8,4 @@ docker build --rm -t copypastabot .
 
 #docker run --rm --name espeakbox -d -p 8080:8080 parente/espeakbox
 
-docker run -d --name copypastabot -t -i --log-driver=journald -v copypasta:/var/ copypastabot
+docker run -d --restart on-failure --name copypastabot -t -i --log-driver=journald -v copypasta:/var/ copypastabot
