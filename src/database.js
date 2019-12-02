1587b01b401180821711870be5c60b0180d44620
STollenaar <stephen@tollenaar.com>
2019-12-02 04:44:24 +0000

    preventing bobby tables

--- src/database.js
+++ src/database.js
@@ -17,7 +17,7 @@ module.exports = {
 		return new Promise((resolve, reject) => {
 			try {
 				db.getConnection((_error, connection) => {
-					connection.query(`SELECT * FROM submissions WHERE id='${postID}';`,
+					connection.query('SELECT * FROM submissions WHERE id=?;', [postID],
 						(_err, results) => resolve(results[0]));
 					connection.release();
 				});
@@ -31,7 +31,7 @@ module.exports = {
 	// Adds the post in the db
 	addPost(postID, title) {
 		db.getConnection((_error, connection) => {
-			connection.query(`INSERT INTO submissions (id, title) VALUES ('${postID}', ${db.escape(title)});`);
+			connection.query('INSERT INTO submissions (id, title) VALUES (?,?);', [postID, title]);
 			connection.release();
 		});
 	},
@@ -47,34 +47,6 @@ module.exports = {
 	},
 
 	removePost(postID) {
-		db.query(`DELETE FROM submissions WHERE id='${postID}';`);
-	},
-
-	getConfigValue(field) {
-		return new Promise((resolve) => {
-			db.getConnection((_error, connection) => {
-				connection.query(`SELECT ${field} FROM config;`, (_err, results) => {
-					let result = results.map((r) => r[field]).join();
-					if (field === 'Debug' || field === 'CensorMode') {
-					// eslint-disable-next-line eqeqeq
-						result = result != 0;
-					}
-					connection.release();
-					resolve(result);
-				});
-			});
-		});
-	},
-
-	setConfigValue(field, value) {
-		if (field === 'Debug' || field === 'CensorMode') {
-			// eslint-disable-next-line eqeqeq
-			// eslint-disable-next-line no-param-reassign
-			value = value ? 1 : 0;
-		}
-		db.getConnection((_error, connection) => {
-			connection.query(`UPDATE config SET ${field}='${value}';`);
-			connection.release();
-		});
+		db.query('DELETE FROM submissions WHERE id=?;', [postID]);
 	},
 };
